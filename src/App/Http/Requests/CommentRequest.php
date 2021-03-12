eac04df8c33014a3ddcbbf34481cdda0ae8b9ef8
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-12 09:24:03 +0000

    Apply fixes from StyleCI

--- src/App/Http/Requests/CommentRequest.php
+++ src/App/Http/Requests/CommentRequest.php
@@ -31,9 +31,8 @@ class CommentRequest extends FormRequest
         ];
     }
 
-
     /**
-     * Dynamically set validator from 'required' to 'sometimes' if resource is being updated
+     * Dynamically set validator from 'required' to 'sometimes' if resource is being updated.
      *
      * @param Validator $validator
      */
